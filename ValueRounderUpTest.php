251d34ab3af05427417de0f3da891c97f31f3249
Adbal99 <adba1999@gmail.com>
2023-04-13 18:17:14 +0200

    added ValueRounderUp class and its test

---
+++ ValueRounderUpTest.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace PragmaGoTech\Interview\Tests;
+
+use PragmaGoTech\Interview\Service\ValueRounderUp;
+use PHPUnit\Framework\TestCase;
+
+class ValueRounderUpTest extends TestCase
+{
+
+}
